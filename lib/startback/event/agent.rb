4e16aa65e6b25171143e42bc00f03b7da91310d2
Bernard Lambeau <blambeau@gmail.com>
2022-05-19 13:36:43 +0200

    Revist the Engine and Agent abstractions to avoid global state.

--- lib/startback/event/agent.rb
+++ lib/startback/event/agent.rb
@@ -1,7 +1,70 @@
 module Startback
   class Event
+    #
+    # An agent listen to specific events and react with its
+    # `call` method.
+    #
+    # This class is intended to be subclasses and the following
+    # methods overriden:
+    #
+    #   - install_listeners that installs sync and async listeners
+    #   - call to create a context and implement reaction behavior
+    #
     class Agent
       include Support::OperationRunner
+      include Support::Robustness
+
+      def initialize(engine)
+        @engine = engine
+        install_listeners
+      end
+      attr_reader :engine
+
+    protected
+
+      # Installs the various event handlers by calling `sync`
+      # and `async` methods.
+      #
+      # This method is intended to be overriden.
+      def install_listeners
+      end
+
+      # Returns the underlying bus
+      def bus
+        engine.bus
+      end
+
+      # Asynchronously listen to a specific event.
+      #
+      # See Bus#listen
+      def async(exchange, queue)
+        bus.async.listen(exchange, queue) do |event|
+          dup.call(event)
+        end
+      end
+
+      # Synchronously listen to a specific event.
+      #
+      # See Bus#listen
+      def sync(exchange, queue)
+        bus.listen(exchange, queue) do |event|
+          dup.call(event)
+        end
+      end
+
+      # Reacts to a specific event.
+      #
+      # This method must be implemented by subclasses and raises
+      # an error by default.
+      def call(event = nil)
+        log(:fatal, {
+          op: self.class,
+          op_data: event,
+          error: %Q{Unexpected call to Startback::Event::Agent#call},
+          backtrace: caller
+        })
+        raise NotImplementedError
+      end
 
     end # class Agent
   end # class Event
--- lib/startback/event/engine.rb
+++ lib/startback/event/engine.rb
@@ -4,10 +4,10 @@ require 'startback'
 module Startback
   class Event
     #
-    # This class runs an infinite loop using ServerEngine.
-    # It is intended to be used to run jobs that listen to
-    # a Startback Bus instance without having the main process
-    # terminating immediately.
+    # This class is the starting point of event handling in
+    # Startback. It holds a Bus instance to which emitters
+    # and listeners can connect, and the possibility for the
+    # the listening part to start an infinite loop (ServerEngine).
     #
     # The Engine automatically runs a Webrick small webapp
     # with a /healthcheck webservice. The class can be extended
@@ -15,86 +15,157 @@ module Startback
     # checks.
     #
     # This class goes hand in hand with the `startback:engine`
-    # docker image.
+    # docker image. It can be extended by subclasses to override
+    # the following methods:
     #
-    # Example:
-    #
-    #     # Dockerfile
-    #     FROM enspirit/startback:engine-0.11
-    #
-    #     # engine.rb
-    #     require 'startback/event/engine'
-    #     Startback::Event::Engine.run
+    #   - bus to use something else than a simple memory bus
+    #   - on_health_check to check specific health conditions
+    #   - create_agents to instantiate all listening agents
+    #     (unless auto_create_agents is used)
     #
     class Engine
+      include Support::Robustness
 
       DEFAULT_OPTIONS = {
-        daemonize: false,
-        worker_type: 'process',
-        workers: 1
+
+        # To be passed to ServerEngine
+        server_engine: {}
+
       }
 
-      def initialize
-        require 'serverengine'
+      def initialize(options = {}, context = Context.new)
+        @options = DEFAULT_OPTIONS.merge(options)
+        @context = context
+      end
+      attr_reader :options, :context
+
+      class << self
+        def auto_create_agents?
+          !!@auto_create_agents
+        end
+
+        # Register a base class which will be used to discover
+        # the agents to start when the engine is ran.
+        def auto_create_agents(base_class = nil)
+          @auto_create_agents ||= base_class
+          @auto_create_agents
+        end
       end
 
+      # This method is executed on health check and can be
+      # overriden by subclasses to perform specific checks.
       def on_health_check
         "Ok"
       end
 
-      def run(options = {})
-        options = DEFAULT_OPTIONS.merge(options)
-        health = Engine.build_health_check(self)
-        worker = Engine.build_worker(health)
-        se = ServerEngine.create(nil, worker, options)
-        se.run
-        se
+      def bus
+        Bus.new
       end
 
-      class << self
-        def run(*args, &bl)
-          new.run(*args, &bl)
+      def connect
+        log(:info, self, "Connecting to the bus now!")
+        bus.connect
+      end
+
+      def run(options = {})
+        connect
+
+        log(:info, self, "Running agents and server engine!")
+        create_agents
+        Runner.new(self, options[:server_engine] || {}).run
+      end
+
+      def create_agents
+        return unless parent = self.class.auto_create_agents
+
+        ObjectSpace
+          .each_object(Class)
+          .select { |klass| klass < parent }
+          .each { |klass| klass.new(self) }
+      end
+
+      class Runner
+
+        DEFAULT_SERVER_ENGINE_OPTIONS = {
+          daemonize: false,
+          worker_type: 'process',
+          workers: 1
+        }
+
+        def initialize(engine, options = {})
+          raise ArgumentError if engine.nil?
+
+          @engine = engine
+          @options = DEFAULT_SERVER_ENGINE_OPTIONS.merge(options)
+          require 'serverengine'
+        end
+        attr_reader :engine, :options
+
+        def run(options = {})
+          health = self.class.build_health_check(engine)
+          worker = self.class.build_worker(engine, health)
+          se = ServerEngine.create(nil, worker, options)
+          se.run
+          se
         end
 
-        def build_health_check(engine)
-          Rack::Builder.new do
-            map '/health-check' do
-              health = Startback::Web::HealthCheck.new {
-                engine.on_health_check
-              }
-              run(health)
-            end
+        class << self
+          def run(*args, &bl)
+            new.run(*args, &bl)
           end
-        end
 
-        def build_worker(health)
-          Module.new do
-            include Support::Env
-
-            def initialize
-              @stop_flag = ServerEngine::BlockingFlag.new
-            end
-
-            define_method(:health) do
-              health
-            end
-
-            def run
-              until @stop_flag.set?
-                Rack::Handler::WEBrick.run(health, {
-                  :Port => env('STARTBACK_ENGINE_PORT', '3000').to_i,
-                  :Host => env('STARTBACK_ENGINE_LISTEN', '0.0.0.0')
-                })
+          def build_health_check(engine)
+            Rack::Builder.new do
+              map '/health-check' do
+                health = Startback::Web::HealthCheck.new {
+                  engine.on_health_check
+                }
+                run(health)
               end
             end
+          end
 
-            def stop
-              @stop_flag.set!
-              Rack::Handler::WEBrick.shutdown
+          def build_worker(engine, health)
+            Module.new do
+              include Support::Env
+
+              def initialize
+                @stop_flag = ServerEngine::BlockingFlag.new
+              end
+
+              define_method(:health) do
+                health
+              end
+
+              define_method(:engine) do
+                engine
+              end
+
+              def run
+                ran = false
+                until @stop_flag.set?
+                  if ran
+                    engine.send(:log, :warn, engine, "Restarting internal loop")
+                  else
+                    engine.send(:log, :info, engine, "Starting internal loop")
+                  end
+                  Rack::Handler::WEBrick.run(health, {
+                    :Port => env('STARTBACK_ENGINE_PORT', '3000').to_i,
+                    :Host => env('STARTBACK_ENGINE_LISTEN', '0.0.0.0')
+                  })
+                  ran = true
+                end
+              end
+
+              def stop
+                engine.send(:log, :info, engine, "Stopping internal loop")
+                @stop_flag.set!
+                Rack::Handler::WEBrick.shutdown
+              end
             end
           end
-        end
-      end # class << self
+        end # class << self
+      end # class Runner
     end # class Engine
   end # class Event
 end # module Startback
