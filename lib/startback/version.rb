7ff5b9d53ff219c8a624842ee1c01cced1abb338
Bernard Lambeau <blambeau@gmail.com>
2023-01-24 12:56:54 +0100

    Bump to 0.16.0 and release.

--- lib/startback/version.rb
+++ lib/startback/version.rb
@@ -1,8 +1,8 @@
 module Startback
   module Version
     MAJOR = 0
-    MINOR = 15
-    TINY  = 5
+    MINOR = 16
+    TINY  = 0
   end
   VERSION = "#{Version::MAJOR}.#{Version::MINOR}.#{Version::TINY}"
 end
